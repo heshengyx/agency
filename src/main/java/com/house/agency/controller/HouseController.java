707df1fd0cc6e3b9c4ab68791661dcd8c3fc1e67
heshengyx <heshengyx@163.com>
2016-04-04 15:59:08 +0800

    1

--- src/main/java/com/house/agency/controller/HouseController.java
+++ src/main/java/com/house/agency/controller/HouseController.java
@@ -2,7 +2,10 @@ package com.house.agency.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.house.agency.param.HouseQueryParam;
@@ -18,6 +21,16 @@ public class HouseController extends BaseController {
 	@RequestMapping("")
 	public String page() {
 		return "house";
+	}
+	
+	@RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
+	public String detail(@PathVariable String id, Model model) {
+		/*Product param = new Product();
+		param.setId(id);
+		param = productService.getData(param);
+		model.addAttribute("product", param);*/
+		model.addAttribute("house", id);
+		return "house-detail";
 	}
 	
 	@RequestMapping("/query")
